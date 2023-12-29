32c823e1ec694ffeb7b34f207531cd3ca283082d
Oksana Moroz <k.moroz.mail@gmail.com>
2023-12-30 01:21:39 +0200

    added section3

---
+++ Section3/challange_second/script.js
@@ -0,0 +1,21 @@
+const calcTip = (bill) => {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [
+  calcTip(bills[0]),
+  calcTip(bills[1]),
+  calcTip(bills[bills.length - 1]),
+];
+const totals = [
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[bills.length - 1] + tips[tips.length - 1],
+];
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
