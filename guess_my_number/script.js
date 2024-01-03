ebdd5b480f518691ca0909bf6122959c73bf1b6d
Oksana Moroz <k.moroz.mail@gmail.com>
2024-01-03 22:37:23 +0200

    fixed

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -1,11 +1,5 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
+
 const x = function () {
   console.log(23);
 };
