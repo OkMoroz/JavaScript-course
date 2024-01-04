85ecd94163adfdca2605c623f6eb42bc00dc872f
Oksana Moroz <k.moroz.mail@gmail.com>
2024-01-04 21:03:37 +0200

    added css style

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -8,10 +8,18 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
+    //When player wins
     document.querySelector('.message').textContent = 'Correct number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -21,6 +29,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
