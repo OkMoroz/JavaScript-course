e048d6c71f55cc156aa560ad91283799c8a5339f
OkMoroz <k.moroz.mail@gmail.com>
2024-02-02 14:04:00 +0200

    calcDisplaySummary

--- arrays/lectures.js
+++ arrays/lectures.js
@@ -240,12 +240,17 @@ GOOD LUCK 😀
 // const adults = humanAges.filter(function (humanAge) {
 //   return humanAge >= 18;
 // });
+//////////
+// const middleAge = adults.reduce((acc, age) => acc + age, 0) / adults.length
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
 
   if (adults.length === 0) return 0;
-  const middleAge = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  const middleAge = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
   console.log('Human Ages:', humanAges);
   console.log('Adults:', adults);
   console.log('Middle Age:', middleAge);
@@ -254,6 +259,24 @@ const calcAverageHumanAge = function (ages) {
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+///////////////////////////////////////
+
+///////////////////////////////////////
+// The Magic of Chaining Methods
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
 
 ///////////////////////////////////////
 // Coding Challenge #3
