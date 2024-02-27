1c140590a4ed163198d66cf6ee19fd4dade2ecb1
OkMoroz <k.moroz.mail@gmail.com>
2024-02-27 16:09:43 +0200

    lat, lng

--- mapty/script.js
+++ mapty/script.js
@@ -10,3 +10,42 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+      //   console.log(map);
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWWidth: 250,
+              minWWidth: 50,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
