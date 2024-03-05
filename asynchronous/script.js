c7a3cb0fc56e4f14a2946602155c35d73030f2da
OkMoroz <k.moroz.mail@gmail.com>
2024-03-05 14:22:00 +0200

    AJAX

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -17,7 +17,7 @@ const getCountryData = function (countries) {
 
       if (data) {
         const html = `
-            <article class="country">
+            <article class="country ${className}">
               <img class="country__img" src="${data.flags.svg}" />
               <div class="country__data">
                 <h3 class="country__name">${data.name.common}</h3>
@@ -47,3 +47,53 @@ const getCountryData = function (countries) {
 
 const countriesToFetch = ['Portugal', 'USA', 'Germany'];
 getCountryData(countriesToFetch);
+///////////////////////////////////////
+// Welcome to Callback Hell
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders?.[0];
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/{neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
