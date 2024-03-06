81912cb057bf60866eb3fbcb0c0717e90374bc80
OkMoroz <k.moroz.mail@gmail.com>
2024-03-06 17:44:39 +0200

    fixed links

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -104,7 +104,7 @@ setTimeout(() => {
 // Throwing Errors Manually
 
 // const getCountryData = function (country) {
-//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
 //     .then(function (response) {
 //       console.log(response);
 //       return response.json();
@@ -117,7 +117,7 @@ setTimeout(() => {
 
 // const getCountryData = function (country) {
 //   // Country 1
-//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
 //     .then(response => {
 //       console.log(response);
 
@@ -134,7 +134,7 @@ setTimeout(() => {
 //       if (!neighbour) return;
 
 //       // Country 2
-//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//       return fetch(`https://restcountries.com/v3.1/alpha/{neighbour}`);
 //     })
 //     .then(response => {
 //       if (!response.ok)
@@ -151,37 +151,3 @@ setTimeout(() => {
 //       countriesContainer.style.opacity = 1;
 //     });
 // };
-const getCountryData = function (country) {
-  // Country 1
-  getJSON(
-    `https://restcountries.eu/rest/v2/name/${country}`,
-    'Country not found'
-  )
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
-
-      if (!neighbour) throw new Error('No neighbour found!');
-
-      // Country 2
-      return getJSON(
-        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
-        'Country not found'
-      );
-    })
-
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      console.error(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-};
-
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
-
-// getCountryData('australia');
