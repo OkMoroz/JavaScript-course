d7d78a215f9cc2bfb1d4fc19bb77836c13384045
OkMoroz <k.moroz.mail@gmail.com>
2024-03-26 16:26:06 +0200

    fixed key

--- forkify/src/js/config.js
+++ forkify/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'Y86c7898a-198e-4a96-89e5-4dc1011dff22';
+export const KEY = '60bea76f-b2e5-4a62-8986-45058db0e24d';
 export const MODAL_CLOSE_SEC = 2.5;
